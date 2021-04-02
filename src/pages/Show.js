aa76e879a883dcd8f5e06a0c670ffbee8a9a72e6
avikkr75 <avikkr75@gmail.com>
2021-04-02 11:52:46 +0530

    added loading and error messages to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,14 +7,40 @@ const Show = () => {
   const { id } = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  let isMounted = true; // React doesn't know,show is unmounted,still try to update the states
 
   useEffect(() => {
-    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is being Loaded...</div>;
+  }
+
+  if (error) {
+    return <div>Error occured : {error}</div>;
+  }
 
   return <div>This is Show Page</div>;
 };
