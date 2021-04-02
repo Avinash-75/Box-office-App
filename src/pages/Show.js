c3f1d8c1b8389fa271f0841210cbec928a8c37ba
avikkr75 <avikkr75@gmail.com>
2021-04-02 22:22:43 +0530

    created show page components and populated them with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/Config';
 
 const reducer = (prevState, action) => {
@@ -64,7 +69,7 @@ const Show = () => {
 
   // console.log('state', state);
 
-  console.log('show', show);
+  // console.log('show', show);
 
   if (isLoading) {
     return <div>Data is being Loaded...</div>;
@@ -74,7 +79,36 @@ const Show = () => {
     return <div>Error occured : {error}</div>;
   }
 
-  return <div>This is Show Page</div>;
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 
 export default Show;
